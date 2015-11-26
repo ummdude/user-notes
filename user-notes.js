8d4b3251950f257c0033e7ce547fb450fad88407
ummdude <greenfishlover@hotmail.com>
2015-11-26 10:23:26 -1000

    edited: html and js, worked a bit on submitting a *new* note. the page hasn't been tested as of yet.

--- user-notes.js
+++ user-notes.js
@@ -1,3 +1,27 @@
+//
+/*
+user-notes.html
+*/
+
+//collection of public notes
+Publicnotes = new Mongo.Collection('publicnote');
+/*
+element attributes:
+  notetitle: notetitle,
+  notetext:
+*/
+
+// to be javascript console?
+// later field: date
+// note text to be stored differently?
+/*
+Publicnotes.insert({
+  note-title: "Test Note"
+  note-text: "test note text"
+  //columns: "1" // how this should be displayed
+});
+*/
+
 Router.configure({
     layoutTemplate: 'main'
 });
@@ -29,10 +53,32 @@ if (Meteor.isClient) {
 
   });
 
-  Template.body.events({
+  Template.create-note.events({
+    // function to create new note
+    'submit form': function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
 
+      // Get value from form element
+      // can't use "-"
+      var notetitle = event.target.notetitle.value;
+      var notetext = event.target.notetext.value;
+      console.log(notetitle);
+      console.log(notetext);
+      // Insert a task into the collection
+      Publicnotes.insert({
+        notetitle: notetitle,
+        notetext: notetext,
+      });
+      // Clear form
+      event.target.notetitle.value = "";
+      event.target.notetext.value = "";
+    }
   });
 
+  Template.view-note.events({
+
+  });
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
   });
