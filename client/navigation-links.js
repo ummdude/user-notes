88b2c7509e196ede497b89d846c4a01c9edb1b11
Grant Shimogawa <gshimoga@gmail.com>
2015-11-27 00:42:22 -1000

    cleaner navigation

---
+++ client/navigation-links.js
@@ -0,0 +1,9 @@
+Template.navigation.helpers({
+
+    pages: [{ route: "home", label: "Home"},
+        { route: "create-note", label:"Create Note" },
+        { route: "public-notes", label: "Public Notes"},
+        { route: "about", label: "About"}
+    ]
+
+});
