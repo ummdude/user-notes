f7160d706fbc8d6662ac2a6e0cf21f6f648e2c2f
Grant Shimogawa <gshimoga@gmail.com>
2015-11-28 12:47:46 -1000

    minor visual improvements

--- client/display-notes-list.js
+++ client/display-notes-list.js
@@ -2,11 +2,4 @@ Template.publicNotes.helpers({
 
     publicNotes: Notes.find({})
 
-    /**
-     [
-     {title: "one", text: "1"},
-     {title: "two", text: "2"},
-     {title: "three", text: "3"}
-     ]
-     */
 });
