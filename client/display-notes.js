be1fb78257546d59a37cf5eef055ee354566eec0
ummdude <colddung@hotmail.com>
2015-12-01 11:59:40 -1000

    Written notes to display-notes.js and user-notes.html; preparation to implement internal links to display-notes page.

--- client/display-notes.js
+++ client/display-notes.js
@@ -1,10 +1,28 @@
 /*
  */
+
+/*
+  this function returns the elements in the Notes collection.
+ */
 Template.publicNotes.helpers({
     publicNotes: Notes.find({})
 });
 
+/*
+  ############## id anchor works ###########
+  ##== internal linking concept:
+  === before a note displays on the display-notes page
+  == using jquery, transform matching text to a page element with a unique name in its "id" tag
+    - matching text is text marked to be "marked" perhaps like ####marker####, an internal link would link to that
+      text.
+  == initially, place internal links in a list to the markers at below the noteTitle, above the noteText
+  ## implementing internal links while writing a note seems to be much more difficult.
+ */
+
 Template.publicNotes.events({
+    /*
+      this function displays the note that is clicked on on the same page.
+     */
     'click a': function(event) {
 
         event.preventDefault();
