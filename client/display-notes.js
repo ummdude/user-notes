e4686bf340ccdc0099148b023dfa6926b2929019
Grant Shimogawa <gshimoga@gmail.com>
2015-12-03 22:15:50 -1000

    Display note information using templates rather than JQuery

--- client/display-notes.js
+++ client/display-notes.js
@@ -1,15 +1,95 @@
-/*
- */
-
-/*
- this function returns the elements in the Notes collection.
- */
 Template.publicNotes.helpers({
-    publicNotes: Notes.find({privacy: "public"})
+
+    /** returns public notes */
+    publicNotes: Notes.find({privacy: "public"}),
+
+    /** returns true if the Session has a current public note */
+    hasPublicNote: function() {
+        if (Session.get("currentPublicNote") == null) {
+            return false;
+        } else {
+            return true;
+        }
+    },
+
+    noteTitle: function() {
+        return (Session.get("currentPublicNote") == null) ?
+            "Untitled" : Session.get("currentPublicNote").title;
+    },
+
+    noteOwner: function() {
+        return (Session.get("currentPublicNote") == null) ?
+            "Anonymous" : ((Meteor.users.findOne(Session.get("currentPublicNote").owner) == null) ?
+            "Anonymous" : Meteor.users.findOne(Session.get("currentPublicNote").owner).username);
+    },
+
+    noteDate: function() {
+        return (Session.get("currentPublicNote") == null) ?
+            "Date" : Session.get("currentPublicNote").createdAt;
+    },
+
+    noteContent: function() {
+        return (Session.get("currentPublicNote") == null) ?
+            "Content" : Session.get("currentPublicNote").text;
+    },
+
+    canDelete: function() {
+        if (Meteor.user() == null) {
+            return false;
+        } else if (Meteor.userId() == Session.get("currentPublicNote").owner) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 });
 
 Template.privateNotes.helpers({
-    publicNotes: Notes.find({privacy: "private", owner: Meteor.userId()})
+
+    /** returns private notes of the current user */
+    privateNotes: Notes.find({privacy: "private", owner: Meteor.userId()}),
+
+    /** returns true if the Session has a current private note */
+    hasPrivateNote: function() {
+        if (Session.get("currentPrivateNote") == null) {
+            return false;
+        } else {
+            return true;
+        }
+    },
+
+    noteTitle: function() {
+        return (Session.get("currentPrivateNote") == null) ?
+            "Untitled" : Session.get("currentPrivateNote").title;
+    },
+
+    noteOwner: function() {
+        return (Session.get("currentPrivateNote") == null) ?
+            "Anonymous" : ((Meteor.users.findOne(Session.get("currentPrivateNote").owner) == null) ?
+            "Anonymous" : Meteor.users.findOne(Session.get("currentPrivateNote").owner).username);
+    },
+
+    noteDate: function() {
+        return (Session.get("currentPrivateNote") == null) ?
+            "Date" : Session.get("currentPrivateNote").createdAt;
+    },
+
+    noteContent: function() {
+        return (Session.get("currentPrivateNote") == null) ?
+            "Content" : Session.get("currentPrivateNote").text;
+    },
+
+    canDelete: function() {
+        if (Meteor.user() == null) {
+            return false;
+        } else if (Meteor.userId() == Session.get("currentPrivateNote").owner) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 });
 
 /*
@@ -24,25 +104,17 @@ Template.privateNotes.helpers({
  */
 
 Template.publicNotes.events({
-    /*
-     this function displays the note that is clicked on on the same page.
-     */
+
     'click a': function(event) {
-
         event.preventDefault();
+        Session.set("currentPublicNote", Notes.findOne(event.target.name));
+    },
 
-        // retrieve note document based on id
-        var search = event.target.name;
-        var searchResult = Notes.findOne(search);
-        var owner = (searchResult.owner == null) ? "Anonymous" : Meteor.users.findOne(searchResult.owner).username;
-
-        // display note information (probably needs to be changed)
-        $("#displayTitle").text(searchResult.title);
-        $("#displayOwner").text(owner);
-        $("#displayDate").text(searchResult.createdAt);
-        $("#displayNote").text(searchResult.text);
-
+    'click button.delete': function(event) {
+        event.preventDefault();
+        // Remove note from collection
     }
+
 });
 
 Template.privateNotes.events({
@@ -50,21 +122,15 @@ Template.privateNotes.events({
      this function displays the note that is clicked on on the same page.
      */
     'click a': function(event) {
-
         event.preventDefault();
+        Session.set("currentPrivateNote", Notes.findOne(event.target.name));
+    },
 
-        // retrieve note document based on id
-        var search = event.target.name;
-        var searchResult = Notes.findOne(search);
-        var owner = (searchResult.owner == null) ? "Anonymous" : Meteor.users.findOne(searchResult.owner).username;
-
-        // display note information (probably needs to be changed)
-        $("#displayTitle").text(searchResult.title);
-        $("#displayOwner").text(owner);
-        $("#displayDate").text(searchResult.createdAt);
-        $("#displayNote").text(searchResult.text);
-
+    'click button.delete': function(event) {
+        event.preventDefault();
+        // Remove note from collection
     }
+
 });
 
 /*
