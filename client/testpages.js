b95771ccc478c6f3f400857201edd65e5e4923d4
ummdude <colddung@hotmail.com>
2015-12-10 12:50:33 -1000

    working on catching special characters

--- client/testpages.js
+++ client/testpages.js
@@ -61,11 +61,11 @@ Template.testPage.events({
      this function displays the note that is clicked on on the same page.
      */
     'click .list-group-item': function(event) {
-      console.log("entered function to display note");
+      // console.log("entered function to display note");
       event.preventDefault();
       Session.set("currentPublicNote", Notes.findOne(event.target.name));
 
-      //using older note retrieval code to obtain note text on correct load time
+      //using older note text retrieval code to obtain note text on correct load time
       var search = event.target.name;
       var searchResult = Notes.findOne(search);
 
@@ -79,9 +79,18 @@ Template.testPage.events({
         // newer display code
         //$("#textElement2").text($("#textElement").text());
       }
+
+      //used to replace code text as string text
+      var quoteRE = new RegExp('"', 'g'); // &#34;
+      var lsThRE = new RegExp('<', 'g'); // &#62;
+      var grThRE = new RegExp('>', 'g'); // &#60;
+      //var lfPaRE = new RegExp(/\(/, 'g'); // &#40; (
+      //var rtPaRE = new RegExp(/\)/, 'g'); // &#41; )
+      var spaceRE = new RegExp(' ', 'g'); //
+
       // creates marker section element and link
       var markerId = "baseID";
-      var markerName = "base marker name";
+      var markerName = "base marker name"; //test special characters
         $.each($("#textElement2").text().split("####"), function( index, value ) {
           if (index == 0) { //creates element of first text section
             if ($("#noteTop").length == 0) {
@@ -89,21 +98,47 @@ Template.testPage.events({
             }
           } else if ((index % 2) == 1) { //all odd indexes is a marker
             markerId = value; //store marker name to use in id and display in element text
-            markerName = markerId;
             // http://stackoverflow.com/questions/1144783/replacing-all-occurrences-of-a-string-in-javascript
-            var re = new RegExp(' ', 'g');
-            markerId = markerId.replace(re, '');
-          } else if ($('#'+markerId).length == 0) { //only if the element does not exist
+            markerName = markerId;
+            // http://stackoverflow.com/questions/9115392/javascript-replace-all-and-in-s-string
+            // markerId = markerId.replace(/["'()<> ]/g,"");
+            markerId = markerId.replace(/ /g, '');
+            markerId = markerId.replace(/"/g, 'Qu');
+            markerId = markerId.replace(/</g, 'LsTh');
+            markerId = markerId.replace(/>/g, 'GrTh');
+            markerId = markerId.replace(/\(/g, 'LfPa');
+            markerId = markerId.replace(/\)/g, 'RtPa');
+            markerId = markerId.replace(/\\/g, 'BkSl');
+            // http://stackoverflow.com/questions/4566771/how-to-globally-replace-a-forward-slash-in-a-javascript-string
+            markerId = markerId.replace(/[/]/g, 'FwSl');
+            // console.log("replaced special characters");
+
+            //replace special chars in name
+            //replace reserved chars in text
+            /*
+            markerName = markerName.replace(/ /g, '');
+            markerName = markerName.replace(/"/g, 'Qu');
+            markerName = markerName.replace(/</g, 'LsTh');
+            markerName = markerName.replace(/>/g, 'GrTh');
+            markerName = markerName.replace(/\(/g, 'LfPa');
+            markerName = markerName.replace(/\)/g, 'RtPa');
+            markerName = markerName.replace(/\\/g, 'BkSl');
+            markerName = markerName.replace(/[/]/g, 'FwSl');
+            */
+
+          } else if ($('#'+markerId+'Marker').length == 0) { //only if the element does not exist
             // reading that &nbsp; should not be used but it's too easier to use.
             $("#noteMarkers").append('<a href="#'+markerId+'Marker">'+markerName+'</a>&nbsp;');
             $("#textDiv").append('<a href="#displayTitle">^Back to Note Title</a>');
+
             value = "=== "+markerName+" ==="+value;
+            value = value.replace(/</g, 'LsTh');
             $("#textDiv").append('<pre id="'+markerId+'Marker">'+value+'</pre>');
           } // end else if markerId3).length
         }); // end for each note "section" in pre modifyPre
         //remove element containing original text
         $("#textElement2").remove();
-        console.log("function display note executed to end");
+        // console.log("function display note executed to end");
     },
 
     'click button.delete': function(event) {
@@ -113,6 +148,15 @@ Template.testPage.events({
         Session.set("currentPublicNote", null);
     }
 });
+
+/*
+replace reserved characters in text and ids
+deal with markers with the same names
+replace function:
+  var re = new RegExp(' ', 'g');
+  markerId = markerId.replace(re, '');
+
+*/
 
 Template.testPageA.helpers({
     // this function returns the public notes in the Notes collection.
